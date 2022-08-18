c1c22de51a908d26e52281d728f8a6894b3af477
Udhayaprakasha <udhaya.venu@anywhere.co>
2022-08-18 21:10:42 +0530

    Time: 9 ms (98.61%), Space: 41.4 MB (97.20%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,29 +1,38 @@
 class Solution {
     public boolean isPalindrome(int x) {
         
-        if(x < 0) {
-            return false;
+        int reverse = 0;
+        int temp = x;
+        while(temp > 0) {
+            reverse *= 10;
+            reverse  += temp % 10;
+            temp = temp / 10;
         }
+        return x == reverse;
         
-        int d = 1;
-        while(x/d >= 10) {
-            d *= 10;
-        }
+//         if(x < 0) {
+//             return false;
+//         }
         
-        while(x != 0) {
-            int q = x / d;
-            int r = x % 10;
-            
-            if(q != r) {
-                return false;
-            }
-            
-            x = (x % d) / 10;
-            
-            d = d / 100;
-        }
+//         int d = 1;
+//         while(x/d >= 10) {
+//             d *= 10;
+//         }
         
-        return true;
+//         while(x != 0) {
+//             int q = x / d;
+//             int r = x % 10;
+            
+//             if(q != r) {
+//                 return false;
+//             }
+            
+//             x = (x % d) / 10;
+            
+//             d = d / 100;
+//         }
+        
+        // return true;
         
         // String num = String.valueOf(x);
         // StringBuilder sb = new StringBuilder();
